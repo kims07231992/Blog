218b1cd1a5c7542f0c142ee77df78731b9281f16
Jason Kim <surferkim@MacBook-Pro.attlocal.net>
2025-01-23 01:02:39 -0800

    Add initial home page

---
+++ surfer-kim-blog/src/app/components/main/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { BannerComponent } from '../../shared/banner/banner.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [BannerComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  bannerTitle = 'Welcome to surfer-kim blog, where words matter.';
+  bannerDescription = 'I\'ll deliver the best stories and ideas on the topics you care about.'
+}
